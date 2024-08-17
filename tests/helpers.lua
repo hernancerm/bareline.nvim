2a33622717c402c2497ef7fbc9bea999c4069bb7
Hernán Cervera <hjcerveram@hotmail.com>
2024-08-16 22:43:12 -0600

    test: increase coverage

--- tests/helpers.lua
+++ tests/helpers.lua
@@ -1,6 +1,6 @@
-local Helpers = {}
+local H = {}
 
-function Helpers.rename_gitdir_to_dotdir()
+function H.rename_gitdir_to_dotdir()
   vim.uv.fs_rename("tests/resources/git_dir_branch/gitdir/",
   "tests/resources/git_dir_branch/.git/")
   vim.uv.fs_rename("tests/resources/git_dir_hash/gitdir/",
@@ -9,7 +9,7 @@ function Helpers.rename_gitdir_to_dotdir()
   "tests/resources/git_dir_tag/.git/")
 end
 
-function Helpers.rename_dotdir_to_gitdir()
+function H.rename_dotdir_to_gitdir()
   vim.uv.fs_rename("tests/resources/git_dir_branch/.git/",
   "tests/resources/git_dir_branch/gitdir/")
   vim.uv.fs_rename("tests/resources/git_dir_hash/.git/",
@@ -18,4 +18,14 @@ function Helpers.rename_dotdir_to_gitdir()
   "tests/resources/git_dir_tag/gitdir/")
 end
 
-return Helpers
+--- Given a string, escape the Lua magic pattern characters so that the string can
+--- be used for an exact match, e.g. as the pattern supplied to 'string.gsub'.
+--- See: https://www.lua.org/manual/5.1/manual.html#5.4.1
+---@param string string
+---@return string
+function H.escape_lua_pattern(string)
+  string, _ = string:gsub("[%(%)%.%%%+%-%*%?%[%]%^%$]", "%%%0")
+  return string
+end
+
+return H
--- tests/test_bareline.lua
+++ tests/test_bareline.lua
@@ -17,20 +17,20 @@
 local new_set = MiniTest.new_set
 local eq = MiniTest.expect.equality
 local child = MiniTest.new_child_neovim()
-local helpers = dofile("tests/helpers.lua")
+local h = dofile("tests/helpers.lua")
 local root_dir = vim.uv.cwd()
 
 local T = new_set({
   hooks = {
     pre_once = function()
-      helpers.rename_gitdir_to_dotdir()
+      h.rename_gitdir_to_dotdir()
     end,
     pre_case = function()
       child.restart({ "-u", "scripts/minimal_init.lua" })
       child.lua([[bareline = require("bareline")]])
     end,
     post_once = function()
-      helpers.rename_dotdir_to_gitdir()
+      h.rename_dotdir_to_gitdir()
       child.stop()
     end,
   },
@@ -47,54 +47,67 @@ T["components"] = new_set({
 
 -- SMOKE
 
-T["components"]["basic startup"] = function()
+T["components"]["smoke"] = function()
   local expected = " NOR  %f  %m  %h  %r%=  tabs:8  %02l,%02c/%02L "
   eq(child.wo.statusline, expected)
 end
 
 -- COMPONENTS
 
--- GIT_HEAD
-
-T["components"]["git_head"] = new_set({
-  parametrize = {
-    {
-      "/tests/resources/git_dir_branch/",
-      " NOR  %f  %m  %h  %r%=  tabs:8  (main)  %02l,%02c/%02L "
-    },
-    {
-      "/tests/resources/git_dir_hash/",
-      " NOR  %f  %m  %h  %r%=  tabs:8  (b1a8f4a)  %02l,%02c/%02L "
-    },
-    {
-      "/tests/resources/git_dir_tag/",
-      " NOR  %f  %m  %h  %r%=  tabs:8  (b1a8f4a)  %02l,%02c/%02L "
-    }
-  }
-})
-
-T["components"]["git_head"]["success"] = function(git_dir, expected_statusline)
-  child.cmd("cd " .. root_dir .. git_dir)
-  eq(child.wo.statusline, expected_statusline)
-end
-
 -- VIM_MODE
 
 T["components"]["vim_mode"] = new_set({
   parametrize = {
-    { "",           "NOR" }, -- Normal
-    { "i",          "INS" }, -- Insert
-    { ":",          "CMD" }, -- Command
-    { "v",          "VIS" }, -- Charwise Visual
-    { "V",          "VIS" }, -- Linewise Visual
-    { "<C-q>",      "VIS" }, -- Block Visual
-    { ":term<CR>a", "TER" }  -- Terminal
+    { "",           "NOR" }, -- Normal.
+    { "i",          "INS" }, -- Insert.
+    { ":",          "CMD" }, -- Command.
+    { "v",          "VIS" }, -- Charwise Visual.
+    { "V",          "VIS" }, -- Linewise Visual.
+    { "<C-q>",      "VIS" }, -- Block Visual.
+    { ":term<CR>a", "TER" }  -- Terminal.
   }
 })
 
 T["components"]["vim_mode"]["success"] = function(keys, expected_vim_mode)
   child.type_keys(keys)
-  eq(string.sub(child.wo.statusline, 2, 4), expected_vim_mode)
+  local vim_mode = string.match(child.wo.statusline, expected_vim_mode)
+  eq(vim_mode, expected_vim_mode)
+end
+
+-- INDENT_STYLE
+
+T["components"]["indent_style"] = new_set({
+  parametrize = {
+    { "",   "",   "tabs:8"   }, -- Nvim defaults.
+    { false, 4,   "tabs:4"   },
+    { true,  2,   "spaces:2" },
+    { true,  4,   "spaces:4" },
+  }
+})
+
+T["components"]["indent_style"]["success"] = function(
+    expandtab, tabstop, expected_indent_style)
+  if expandtab ~= "" then child.bo.expandtab = expandtab end
+  if tabstop ~= "" then child.bo.tabstop = tabstop end
+  local indent_style = string.match(child.wo.statusline, expected_indent_style)
+  eq(indent_style, expected_indent_style)
+end
+
+-- GIT_HEAD
+
+T["components"]["git_head"] = new_set({
+  parametrize = {
+    { "/tests/resources/git_dir_branch/", "(main)"    },
+    { "/tests/resources/git_dir_hash/",   "(b1a8f4a)" },
+    { "/tests/resources/git_dir_tag/",    "(b1a8f4a)" },
+  }
+})
+
+T["components"]["git_head"]["success"] = function(git_dir, expected_git_head)
+  child.cmd("cd " .. root_dir .. git_dir)
+  local git_head = string.match(
+    child.wo.statusline, h.escape_lua_pattern(expected_git_head))
+  eq(git_head, expected_git_head)
 end
 
 return T
