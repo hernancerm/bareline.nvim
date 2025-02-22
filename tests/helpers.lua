7cb066f22f83a1829f6841a2bceadb7a4430394b
Hernán Cervera <hjcerveram@hotmail.com>
2025-02-22 14:49:17 -0600

    refactor(components.git_head)!: async search from buf name
    BREAKING CHANGES:
    
    - Changes in field `opts` from `bareline.BareComponent`:
      - `opts.watcher.autocmd` renamed to `opts.redraw_on_autocmd`.
      - `opts.watcher.filepath` removed. Use instead `bareline.redraw_on_fs_event()`.
    - The component `git_head` now requires Git to be installed.

--- tests/helpers.lua
+++ tests/helpers.lua
@@ -1,34 +1,59 @@
 local h = {}
 
 h.resources_dir = vim.uv.cwd() .. "/tests/resources"
+h.tmp_testing_dir = vim.fn.stdpath("data") .. "/bareline.nvim/tmp_testing_dir"
 
-function h.rename_gitdir_to_dotdir()
+--- It seems necessary to call sleep in a loop to go through several event loop cycles, so the
+--- values of the vars of async components have an opportunity to be computed.
+---@param child any Return value from `MiniTest.new_child_neovim()`
+---@param milliseconds integer
+---@param times integer
+function h.sleep(child, milliseconds, times)
+  for _ = 1, times do
+    child.lua("vim.uv.sleep(" .. milliseconds .. ")")
+  end
+end
+
+---@param child any Return value from `MiniTest.new_child_neovim()`
+function h.get_child_evaluated_stl(child)
+  return child.lua_get("vim.api.nvim_eval_statusline(vim.wo.statusline, {}).str")
+end
+
+function h.rename_git_dirs_for_testing()
   vim.uv.fs_rename(
-    h.resources_dir .. "/git_dir_branch/gitdir",
+    h.resources_dir .. "/git_dir_branch/dot_git",
     h.resources_dir .. "/git_dir_branch/.git"
   )
   vim.uv.fs_rename(
-    h.resources_dir .. "/git_dir_hash/gitdir",
+    h.resources_dir .. "/git_dir_branch_no_showUntrackedFiles/dot_git",
+    h.resources_dir .. "/git_dir_branch_no_showUntrackedFiles/.git"
+  )
+  vim.uv.fs_rename(
+    h.resources_dir .. "/git_dir_hash/dot_git",
     h.resources_dir .. "/git_dir_hash/.git"
   )
   vim.uv.fs_rename(
-    h.resources_dir .. "/git_dir_tag/gitdir",
+    h.resources_dir .. "/git_dir_tag/dot_git",
     h.resources_dir .. "/git_dir_tag/.git"
   )
 end
 
-function h.rename_dotdir_to_gitdir()
+function h.rename_git_dirs_for_tracking()
   vim.uv.fs_rename(
     h.resources_dir .. "/git_dir_branch/.git",
-    h.resources_dir .. "/git_dir_branch/gitdir"
+    h.resources_dir .. "/git_dir_branch/dot_git"
+  )
+  vim.uv.fs_rename(
+    h.resources_dir .. "/git_dir_branch_no_showUntrackedFiles/.git",
+    h.resources_dir .. "/git_dir_branch_no_showUntrackedFiles/dot_git"
   )
   vim.uv.fs_rename(
     h.resources_dir .. "/git_dir_hash/.git",
-    h.resources_dir .. "/git_dir_hash/gitdir"
+    h.resources_dir .. "/git_dir_hash/dot_git"
   )
   vim.uv.fs_rename(
     h.resources_dir .. "/git_dir_tag/.git",
-    h.resources_dir .. "/git_dir_tag/gitdir"
+    h.resources_dir .. "/git_dir_tag/dot_git"
   )
 end
 
@@ -58,6 +83,30 @@ function h.remove_git_worktree()
       h.resources_dir .. "/git_dir_branch_worktree",
     })
     :wait()
+end
+
+function h.create_tmp_testing_dir()
+  vim.fn.mkdir(h.tmp_testing_dir, "p")
+  vim.fn.mkdir(h.tmp_testing_dir .. "/sub_dir_a/sub_dir_a_a/sub_dir_a_a_a", "p")
+  vim.fn.mkdir(h.tmp_testing_dir .. "/sub_dir_b/sub_dir_b_b/sub_dir_b_b_b", "p")
+  vim.fn.writefile({}, h.tmp_testing_dir .. "/file.txt")
+  vim.fn.writefile({}, h.tmp_testing_dir .. "/untracked.txt")
+  vim.fn.writefile({}, h.tmp_testing_dir .. "/sub_dir_a/sub_dir_a_a/file.txt")
+  vim.fn.writefile({}, h.tmp_testing_dir .. "/sub_dir_b/sub_dir_b_b/sub_dir_b_b_b/file.txt")
+end
+
+function h.delete_tmp_testing_dir()
+  vim.fn.delete(h.tmp_testing_dir .. "/file.txt")
+  vim.fn.delete(h.tmp_testing_dir .. "/untracked.txt")
+  vim.fn.delete(h.tmp_testing_dir .. "/sub_dir_a/sub_dir_a_a/file.txt")
+  vim.fn.delete(h.tmp_testing_dir .. "/sub_dir_b/sub_dir_b_b/sub_dir_b_b_b/file.txt")
+  vim.fn.delete(h.tmp_testing_dir .. "/sub_dir_a/sub_dir_a_a/sub_dir_a_a_a", "d")
+  vim.fn.delete(h.tmp_testing_dir .. "/sub_dir_a/sub_dir_a_a", "d")
+  vim.fn.delete(h.tmp_testing_dir .. "/sub_dir_a", "d")
+  vim.fn.delete(h.tmp_testing_dir .. "/sub_dir_b/sub_dir_b_b/sub_dir_b_b_b", "d")
+  vim.fn.delete(h.tmp_testing_dir .. "/sub_dir_b/sub_dir_b_b", "d")
+  vim.fn.delete(h.tmp_testing_dir .. "/sub_dir_b", "d")
+  vim.fn.delete(h.tmp_testing_dir, "d")
 end
 
 --- Given a string, escape the Lua magic pattern characters so that the string can
